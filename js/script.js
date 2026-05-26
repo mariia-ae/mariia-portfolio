d3c78c8d2b4f6d888beab35aae502ba3a9becadb
MariiaCh21 <chertkovamasha21@icloud.com>
2026-05-26 14:26:28 +0200

    Upgrade

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const text = "Offen für Praktikumsmöglichkeiten und neue technische Herausforderungen.";
+const typingElement = document.getElementById("typing-text");
+
+let index = 0;
+let isDeleting = false;
+function typeEffect() {
+    if (!isDeleting) {
+        typingElement.textContent = text.substring(0, index);
+        index++;
+        if (index > text.length) {
+            isDeleting = true;
+            setTimeout(typeEffect, 3500);
+            return;
+        }
+    } else {
+        typingElement.textContent = text.substring(0, index);
+        index--;
+        if (index < 0) {
+            isDeleting = false;
+        }
+    }
+    const speed = isDeleting ? 55 : 95;
+    setTimeout(typeEffect, speed);
+}
+typeEffect();
